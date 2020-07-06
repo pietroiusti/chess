2e27c4d804729b2ed1e3556a2a1196507770f42e
glptrst <giulio.pietroiusti@gmail.com>
2020-07-06 22:38:04 +0200

    Change error message.

--- index.js
+++ index.js
@@ -7,54 +7,57 @@ const { Chess } = require('./chess.js');
 const port = process.env.PORT || 3000;
 
 let server = http.createServer(function (request, response) {
-    console.log('request ', request.url);
+  console.log('request ', request.url);
 
-    var filePath = '.' + request.url;
-    if (filePath == './') {
-        filePath = './public/index.html';
-    } else {
-      filePath = './public/' + filePath;
+  var filePath = '.' + request.url;
+  if (filePath == './') {
+    filePath = './public/index.html';
+  } else {
+    filePath = './public/' + filePath;
+  }
+
+  var extname = String(path.extname(filePath)).toLowerCase();
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+  };
+
+  var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if(error.code == 'ENOENT') {
+        // fs.readFile('./404.html', function(error, content) {
+        //   response.writeHead(404, { 'Content-Type': 'text/html' });
+        //   response.end(content, 'utf-8');
+        // });
+	console.log(error);
+	response.writeHead(400);
+	response.end('Error: ENOENT: no such file or directory');
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+      }
     }
-
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
-    };
-
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
 });
 
 server.listen(port, () => {
